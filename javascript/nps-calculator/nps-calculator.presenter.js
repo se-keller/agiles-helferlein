dec4bd4058d1120a329888c0ca967b6462dcefd2
se-keller <sebastian.sekell@googlemail.com>
2014-12-02 15:37:38 +0100

    nur der gh-pages branch soll genutzt werden

--- javascript/nps-calculator/nps-calculator.presenter.js
+++
@@ -1,7 +0,0 @@
-function nps() {
-	var promoter = parseInt(document.getElementById("promoter-input").value);
-	var indifferent = parseInt(document.getElementById("indifferent-input").value);
-	var detractor = parseInt(document.getElementById("detractor-input").value);
-    var nps = calculateNps(promoter, indifferent, detractor);
-	document.getElementById("nps-label").innerHTML = "NPS = " + nps + " %";
-}
